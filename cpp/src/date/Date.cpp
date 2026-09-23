2a7886263dde3057e6032feda2d6259128060b4c
Srinjena Das <dassrinjena@gmail.com>
2026-09-24 01:25:26 +0530

    Add Date Validation

--- cpp/src/date/Date.cpp
+++ cpp/src/date/Date.cpp
@@ -1,9 +1,53 @@
 #include "qe/date/Date.hpp"
+#include <stdexcept>
+
+bool isLeapYear(int year)
+{
+    return (year%400==0) || ((year%4==0) && (year%100 !=0));
+
+}
+
+int daysInMonth(int month, int year)
+{   
+    if (month <1 || month >12)
+    {
+        throw std::invalid_argument("Invalid Month");
+    }
+
+    if (((month<=7) && (month%2!=0)) || ((month>=8) && (month%2==0)))
+        return 31;
+    else if (((month<=6) && (month%2==0) && (month!=2)) || ((month>=9) && (month%2!=0)))
+        return 30;
+    else
+    {
+        if (isLeapYear(year))
+            return 29;
+        else
+            return 28;
+    }
+
+}
+
+
+void validateDate(int day, int month, int year)
+{
+    if (year<=0)
+    {
+        throw std::invalid_argument("Invalid Year");
+    }
+    int max_day=daysInMonth(month, year);
+
+    if (day<1 || day > max_day)
+    {
+        throw std::invalid_argument("Invalid Day");
+    }
+}
+
 
 Date:: Date(int day, int month, int year): day_(day), month_(month), year_(year)
 
 {
-
+    validateDate(day, month, year);
 }
 
 int Date::day() const
@@ -20,3 +64,4 @@ int Date::year() const
 {
     return year_;
 }
+
--- tests/test_date.cpp
+++ tests/test_date.cpp
@@ -1,5 +1,6 @@
 #include "qe/date/Date.hpp"
 #include <cassert>
+#include<stdexcept>
 
 int main()
 {
@@ -12,6 +13,40 @@ int main()
     assert(today.day()==23);
     assert(today.month()==9);
     assert(today.year()==2026);
-    
+
+    Date leapDay(29, 2, 2024);
+
+    assert(leapDay.day() == 29);
+    assert(leapDay.month() == 2);
+    assert(leapDay.year() == 2024);
+
+    try
+    {
+        Date badDate(29,2,2025);
+        assert(false);
+    }
+    catch (const std::invalid_argument&)
+    {
+
+    }
+
+    try
+    {
+        Date badMonth(10, 13, 2026);
+        assert(false);
+    }
+    catch (const std::invalid_argument&)
+    {
+    }
+
+    try
+    {
+        Date badDay(31, 4, 2026);
+        assert(false);
+    }
+    catch (const std::invalid_argument&)
+    {
+    }
+        
     return 0;
 }
